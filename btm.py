34d4073dee850716ebe03abf55c212aeec3b7fd1
TimovNiedek <timovanniedek@me.com>
2017-06-13 11:15:26 +0200

    BTM for reddit 2017

--- btm.py
+++ btm.py
@@ -182,7 +182,7 @@ class BitermTopicModel:
             return ret
 
 if __name__=="__main__":
-    with open('headlines100K.txt') as f:
+    with open('headlines2017.txt') as f:
         doc=f.read()
         
     vocab_size = 5000
@@ -195,12 +195,12 @@ if __name__=="__main__":
         
     # Expected running time: 3h
     
-    btm = BitermTopicModel(split[:10000], vocab_size)
+    btm = BitermTopicModel(split, vocab_size)
     #btm.fit(num_iterations=250, num_topics=20,alpha=2.5,beta=0.01)
-    btm.load_params('btm_params_100K.npz')
+    btm.load_params('btm_params_2017.npz')
     for i, topic in enumerate(btm.get_topic_words(words_per_topic=20)):
         print("Topic {}: ".format(i), topic)
-    topic_probs = btm.get_topics_for_docs(split[20000:20010])
+    topic_probs = btm.get_topics_for_docs(split[0:10])
     for i, probs in enumerate(topic_probs):
-        print("\""+unique_headlines[20000+i]+"\": topic {}".format(np.argmax(probs)))
-    btm.save_params('btm_params_100K.csv')
+        print("\""+unique_headlines[i]+"\": topic {}".format(np.argmax(probs)))
+    #btm.save_params('btm_params_2017')
